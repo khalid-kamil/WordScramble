74c94ef2a6af05ca970a583bf5d41a6dd8b663de
Khalid Kamil <carts_carps_0o@icloud.com>
2022-01-20 00:00:31 +0000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Khalid Kamil on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
