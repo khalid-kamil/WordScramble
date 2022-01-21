0e620c934bba75b4da8ae475320fdb9ceec58331
Khalid Kamil <carts_carps_0o@icloud.com>
2022-01-21 19:38:25 +0000

    1. Disallow answers that are shorter than three letters or are just our start word. 2. Add a toolbar button that calls startGame(), so users can restart with a new word whenever they want to. 3. Put a text view somewhere so you can track and show the player’s score for a given root word. How you calculate score is down to you, but something involving number of words and their letter count would be reasonable.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -5,17 +5,190 @@
 //  Created by Khalid Kamil on 20/01/2022.
 //
 
+//if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+//    // We found the file in our bundle
+//}
+//    if let fileContents = try? String(contentsOf: fileURL) {
+//        // we loaded the file into a string!
+//    }
+//        let letters = fileContents.components(separatedBy: "\n")
+//        let letter = letters.randomElement()
+//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+//        ///            Checking a string for misspelled words
+//        ///1. Create a word to check and an instance of UITextChecker to check the string
+//        let word = "swift"
+//        let checker = UITextChecker()
+//        ///            2. Tell the checker how much of our string we want to check. Need to ask Swift to create an Objective-C string range using the entire length of all our characters
+//        let range = NSRange(location: 0, length: word.utf16.count)
+//        /// 3. We can ask our text checker to report where it found any misspellings in our word, passing in the range to check, a position to start within the range (so we can do things like “Find Next”), whether it should wrap around once it reaches the end, and what language to use for the dictionary
+//        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+//        /// That sends back another Objective-C string range, telling us where the misspelling was found. Even then, there’s still one complexity here: Objective-C didn’t have any concept of optionals, so instead relied on special values to represent missing data.
+//        /// In this instance, if the Objective-C range comes back as empty – i.e., if there was no spelling mistake because the string was spelled correctly – then we get back the special value NSNotFound.
+//        let allGood = misspelledRange.location == NSNotFound
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var score = 0
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Try a new word") {
+                        startGame()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Text("Score: \(score)")
+                }
+                
+            }
+        }
+        .onSubmit(addNewWord)
+        .onAppear(perform: startGame)
+        .alert(errorTitle, isPresented: $showingError) {
+            Button("OK", role: .cancel) { }
+        } message: {
+            Text(errorMessage)
+        }
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            newWord = ""
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            newWord = ""
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            newWord = ""
+            return
+        }
+        
+        guard isLongEnough(word: answer) else {
+            wordError(title: "Word is too short", message: "Your word needs to contain at least 3 characters")
+            newWord = ""
+            return
+        }
+        
+        guard isNotStartWord(word: answer) else {
+            wordError(title: "Word is start word", message: "Come up with your own word")
+            newWord = ""
+            return
+        }
+        
+        withAnimation{
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+        score = score + answer.count
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                // If we are here everything has worked, so we can exit
+                newWord = ""
+                usedWords = [String]()
+                score = 0
+                return
+            }
+        }
+        
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isLongEnough(word: String) -> Bool {
+        word.count >= 3
+    }
+    
+    func isNotStartWord(word: String) -> Bool {
+        !(word == rootWord)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().colorScheme(.dark)
     }
 }
